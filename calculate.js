041eeade9745864afc3bf6279ca7ae2853e82427
linalumburovska <linalumburovska20@gmail.com>
2019-01-30 11:50:09 +0100

    Added operators and numbers of the calculator, functionality still not working

--- calculate.js
+++ calculate.js
@@ -13,3 +13,17 @@ function multiply(a,b){
 function divide(a,b){
     return a/b;
 }
+
+function operate(operator,a,b){
+    var result='';
+    if(operator=='plus'){
+        result=add(a,b);
+    }else if(operator=='minus'){
+        result=subtract(a,b);
+    }else if(operator=='product'){
+        result=multiply(a,b);
+    }else if(operator=='quotient'){
+        result=divide(a,b);
+    }
+    return result;
+}
